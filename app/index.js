e42601fcfdb7032784885da140a1af38f7814536
AeliaWin <u6540064@au.edu>
2025-01-25 21:45:40 +0700

    setup for notifications

--- app/index.js
+++ app/index.js
@@ -8,13 +8,57 @@ import * as SplashScreen from 'expo-splash-screen';
 import { View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { auth } from './config/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
 // import { Provider } from 'react-redux';
 // import { store } from './redux/store';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: true,
+  }),
+});
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if(Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    alert('Failed to get push token for push notification!');
+    return;
+  }
+
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log('Push token:', token);
+  return token;
+}
 export default function App() {
   const [isReady, setIsReady] = React.useState(false);
   const [user, setUser] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
+  const [expoPushToken, setExpoPushToken] = React.useState('');
+  const [channels, setChannels] = React.useState([]);
+  const [notification, setNotification] = React.useState(undefined);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();  
   useEffect(() => {
     const prepareApp = async () => {
       SplashScreen.preventAutoHideAsync();
@@ -22,7 +66,25 @@ export default function App() {
       setIsReady(true);
       SplashScreen.hideAsync();
     }
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    if (Platform.OS === 'android') {
+      Notifications.getNotificationChannelsAsync().then(channels => setChannels(channels));
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log("Notification response received:", response);
+      });
+    }
     prepareApp();
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
   }, []);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
